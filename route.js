2ede9163b105461e318f7f06dbf4248026425a69
radityapujamurti <radityapujamurti@gmail.com>
2016-04-18 23:47:08 +0800

    configure admin route

--- route.js
+++ route.js
@@ -1,4 +1,16 @@
-Router.route('/admin');
+
+Router.route('/admin', {
+    name: 'Admin',
+    template: 'admin',
+    onBeforeAction: function(){
+        var currentUser = Meteor.userId();
+        if(currentUser == "DAevKXNQH9FcFKdPH"){
+            this.next();
+        } else {
+            this.render("home");
+        }
+    }
+});
 
 Router.route('/',{
 	template: 'home'
