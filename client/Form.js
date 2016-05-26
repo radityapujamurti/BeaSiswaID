bbe2175da47b34d1f54a3f7bfcc01938a1db8625
radityapujamurti <radityapujamurti@gmail.com>
2016-05-26 14:20:50 +0800

    restructured the files

---
+++ client/Form.js
@@ -0,0 +1,70 @@
+
+Template.addPostForm.events({
+    "submit .new-post": function (event) {
+      // Prevent default browser form submit
+      event.preventDefault();
+
+      // Get value from form element
+      var title = event.target.title.value;
+      var eligibility = event.target.eligibility.value;
+      var description = event.target.description.value;
+      var deadline = event.target.deadline.value;
+      var location = event.target.location.value;
+      var link = event.target.link.value;
+ 
+      // Insert a task into the collection
+      Meteor.call("addPost", title, eligibility, description, deadline, location, link);
+ 
+      // Clear form
+      event.target.title.value= "";
+      event.target.eligibility.value= "";
+      event.target.description.value= "";
+      event.target.deadline.value= "";
+      event.target.location.value= "";
+      event.target.link.value= "";
+
+      alert("Thank you! Your post will be reviewed.");
+
+    }
+  });
+
+Template.addReviewForm.events({
+  "submit .new-review": function (event) {
+      // Prevent default browser form submit
+      event.preventDefault();
+
+      // Get value from form element
+      var title = event.target.title.value;
+      var location = event.target.location.value;
+      var content = event.target.content.value;
+      var preview = content.substr(0,100)+'...';
+
+      // Insert a task into the collection
+      Meteor.call("addReview", title, location, content, preview);
+ 
+      // Clear form
+      event.target.title.value= "";
+      event.target.location.value= "";
+      event.target.content.value= "";
+      alert("Thank you! Your story will be reviewed.");
+    }
+});
+
+Template.editReviewForm.events({
+  "submit .edit-review": function (event) {
+      // Prevent default browser form submit
+      event.preventDefault();
+
+      // Get value from form element
+      var title = event.target.title.value;
+      var location = event.target.location.value;
+      var content = event.target.content.value;
+      var preview = content.substr(0,100)+'...';
+
+      // Insert a task into the collection
+      Meteor.call("editReview", this._id, title, location, content, preview);
+ 
+      alert("Your story has been updated!");
+  }
+});
+
